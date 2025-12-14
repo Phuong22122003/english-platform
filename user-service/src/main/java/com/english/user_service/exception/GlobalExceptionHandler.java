b73d007f01dcff32ddaf1999559a9d70e6c6d351
Phuong221203 <hnguyenphuong09@gmail.com>
2025-12-14 23:28:43 +0700

    update: config

---
+++ user-service/src/main/java/com/english/user_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,63 @@
+package com.english.user_service.exception;
+
+
+import com.english.dto.response.ApiResponse;
+import com.english.exception.AppException;
+import com.english.exception.BadRequestException;
+import com.english.exception.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ApiResponse<?>> handlerBadRequest(BadRequestException ex){
+        return ResponseEntity.badRequest().body(
+                ApiResponse.builder()
+                        .code(HttpStatus.BAD_REQUEST.value())
+                        .message(ex.getMessage())
+                        .build());
+    }
+
+    @ExceptionHandler(value = RuntimeException.class)
+    ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException runtimeException) {
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        apiResponse.setMessage("Server error. Please try again");
+        return  ResponseEntity.internalServerError().body(apiResponse);
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ApiResponse<?>> handlerNotFoundException(NotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                ApiResponse.builder()
+                        .code(HttpStatus.NOT_FOUND.value())
+                        .message(ex.getMessage())
+                        .build());
+    }
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ApiResponse<?>> handlerUnauthorizedException(NotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                ApiResponse.builder()
+                        .code(HttpStatus.UNAUTHORIZED.value())
+                        .message(ex.getMessage())
+                        .build());
+    }
+
+    @ExceptionHandler(AppException.class)
+    public ResponseEntity<?> handelForAppException(AppException ex){
+        return ResponseEntity.badRequest().body(
+                ApiResponse.builder()
+                        .code(ex.getCode())
+                        .message(ex.getMessage())
+                        .build());
+    }
+}
